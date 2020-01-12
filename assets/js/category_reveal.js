e7ca6ec9ebddc248e689977b57636d729ef9987e
Toomber <58906286+Toomber@users.noreply.github.com>
2020-01-12 22:37:19 +1100

    Categories Upgrade
    Made category tags a html file to be included
    Made tag of current category a different style and unselectable
    Added category tags to main menu banner and changed nav button to daisable/enable them with javascript

---
+++ assets/js/category_reveal.js
@@ -0,0 +1,9 @@
+
+function reveal_categories() {
+  var x = document.getElementById("revealable_div");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
